9484251e2e1dbb14aad09b69c47d3e219cd9e348
Paul Tikken Laptop <paul.tikken@gmail.com>
2021-02-19 09:56:05 +0000

    reconfigured flask to docker-compose.yml

---
+++ manager.py
@@ -0,0 +1,24 @@
+from flask_script import Manager
+
+from set_version import VERSION
+from spectacles.webapp.run import create_app
+
+__version__ = VERSION
+
+app = create_app(version=__version__)
+
+manager = Manager(app)
+
+
+@manager.command
+def runserver():
+    app.run(host="0.0.0.0", port=5000)
+
+
+@manager.command
+def runworker():
+    app.run(debug=False)
+
+
+if __name__ == "__main__":
+    manager.run()
